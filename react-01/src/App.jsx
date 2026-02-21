64fedf50891627c9e1f5cea3ebcd7a8af3c9be25
Akash9634 <akashkush2020@gmail.com>
2026-02-21 21:26:57 +0530

    first commit

---
+++ react-01/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from "./components/Card"
+
+function App() {
+  
+
+  return (
+    <>
+      <div>Hello World</div>
+      <Card />
+    </>
+  )
+}
+
+export default App
