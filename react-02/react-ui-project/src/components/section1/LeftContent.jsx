f2f6c7f92ff009199997b7be1f403888e2b41e94
Akash9634 <akashkush2020@gmail.com>
2026-02-25 11:35:45 +0530

    card project

---
+++ react-02/react-ui-project/src/components/section1/LeftContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/3 flex flex-col justify-between'>
+      <div className='p-6'>
+       <h3 className='mb-7 text-5xl font-bold'>Prospective <br /> <span>Customer</span>  <br /> Segmentation</h3>
+       <p className='text-xl font-medium text-gray-400'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem odit perferendis dolor molestiae recusandae reiciendis!</p>
+       
+      </div>
+      <div className='text-8xl'>
+        <i className="ri-arrow-right-up-long-fill"></i>
+      </div>
+    </div>
+  )
+}
+
+export default LeftContent
---
+++ react-02/react-ui-project/src/components/section1/section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './navbar.jsx'
+import Page1Content from './Page1Content.jsx'
+const section1 = () => {
+  return (
+    <div className='h-screen w-full '>
+      <Navbar />
+      <Page1Content />
+    </div>
+  )
+}
+
+export default section1
